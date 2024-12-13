cf4168ba5df7b19fa1690e854022b63cccabdcc3
mhd-radin <radinpaliyalill@gmail.com>
2024-12-13 20:30:13 +0530

    post backend updated

--- events/js/events.app.js
+++ events/js/events.app.js
@@ -3,13 +3,28 @@ bushido.getCollection("posts").then(function (snapshot) {
   useEmptyInfoScreen();
   arr.forEach(function (dt, index) {
     var data = dt.data();
-    var tagstring = useImageCard(
-      data.id,
-      data.title,
-      data.des,
-      data.imgUrl,
-      false
-    );
-    addPostToBody(tagstring);
+    var thumbImg = data.imgUrl;
+
+    function addPost() {
+      var tagstring = useImageCard(
+        data.id,
+        data.title,
+        data.des,
+        thumbImg,
+        false
+      );
+      addPostToBody(tagstring);
+    }
+
+    if (data.imageType == "file") {
+        
+      bushido.get("base64", data.imgUrl).then(function (snapshot) {
+        var data = snapshot.data();
+        thumbImg = data.url;
+        addPost();
+      });
+    } else {
+      addPost();
+    }
   });
 });

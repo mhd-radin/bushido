54828fb3b2f02b21e962b74ce83f130c38f1ec47
mhd-radin <radinpaliyalill@gmail.com>
2025-01-31 14:28:43 +0530

    profile link admin. status updation for admin bot. navbar responsive. style bug fix

---
+++ Admin/notifier/js/botinfo.js
@@ -0,0 +1,55 @@
+let sentCount = id('sentCount');
+let statusInfo = id('statusInfo');
+let cli = id('cliInfo');
+let botInfo = id('activeInfo');
+
+function createActiveInfoDot(isRed, text) {
+  return `<div class="green-dot ${(isRed? 'red-color' : '')}"></div> ${text}`
+}
+
+function getInfo() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://wbot-bodg.onrender.io/status');
+  xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status == 200) {
+        var res = xhr.response;
+        if (typeof res === 'string') {
+          res = JSON.parse(res);
+        }
+
+        if (res.isClientLogged === true) {
+          handleBotStatus(res.sentCount + '+', 'Messages are sent', true, true)
+        } else {
+          handleBotStatus('Warning', 'No client logged', false, true)
+        }
+      } else {
+        handleBotStatus('ERROR', 'Servive currently unavailable', false, false)
+      }
+    }
+  })
+  xhr.send()
+}
+
+function handleBotStatus(sentCountInfo, sentDes, cliInfo = true, active) {
+  var cliText = '';
+  var activeText = '';
+  if (cli === true) {
+    cliText = 'Client Logged'
+  } else {
+    cliText = 'No Client Found!'
+  }
+
+  if (active === true) {
+    activeText = 'Active'
+  } else {
+    activeText = 'No Service'
+  }
+
+  sentCount.innerHTML = sentCountInfo;
+  statusInfo.innerHTML = sentDes;
+  cli.innerHTML = cliText;
+  botInfo.innerHTML = createActiveInfoDot((!active), activeText)
+}
+
+getInfo();

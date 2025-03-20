c872a323d56dac46c27e3294be69ed0ea1fba4e2
mhd-radin <radinpaliyalill@gmail.com>
2025-03-20 19:18:31 +0530

    update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -13,9 +13,9 @@ function pageonload() {
     eva.replace();
   }
 
-  // document.querySelectorAll("*").forEach(function(elem) {
-  //   elem.onerror = handleError;
-  // });
+   document.querySelectorAll("*").forEach(function(elem) {
+    elem.onerror = handleError;
+  });
   document.documentElement.style.setProperty("--animation-state", "running");
 }
 
